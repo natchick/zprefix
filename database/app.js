6e961ed8e342697b9a6125ceb0e3ffef98adf154
natchick <natashachick77@gmail.com>
2023-12-05 16:36:18 -1000

    CRUD ops complete on database

--- database/app.js
+++ database/app.js
@@ -73,3 +73,76 @@ app.delete('/items/:id', (req, res) => {
 })
 
 //update method
+app.patch('/items/:id', (req, res) => {
+    knex('item_table')
+        .where('id', req.params.id)
+        .update({
+            'Quanitity': req.params.Quanitity
+        })
+        .then(() => res.json(newItem));
+})
+
+
+//get users
+app.get('/users', (req, res) => {
+    knex('user_table')
+        .select('*')
+        .then(users => {
+            res.json(users);
+        });
+});
+
+//get users by id 
+app.get('/users/:id', (req, res) => {
+    var { id } = req.params;
+    knex('user_table')
+        .select('*')
+        .where('id', id)
+        .then(users => {
+            res.json(users);
+        });
+});
+
+//post request to post a new user
+app.post('/users', async(req, res) => {
+    const maxIdQuery = await knex('user_table').max('id as maxId').first();
+
+    await knex('user_table').insert({
+            id: maxIdQuery.maxId + 1,
+            First_Name: req.body.First_Name,
+            Last_Name: req.body.Last_Name,
+            Username: req.body.Username,
+            Password: req.body.Password
+        })
+        .then(() => {
+            knex('user_table')
+            .select('*')
+            .then(user=> {
+                res.json(user);
+            })
+        })
+});
+
+//delete method
+app.delete('/users/:id', (req, res) => {
+    knex('user_table')
+    .where('id', req.params.id)
+    .del()
+    .then(function(){
+        knex('user_table')
+            .select('*')
+            .then(users => {
+                res.json(users);
+            })
+    })
+})
+
+//update method
+app.patch('/users/:id', (req, res) => {
+    knex('user_table')
+        .where('id', req.params.id)
+        .update({
+            'Password': req.params.Password
+        })
+        .then(() => res.json(newPassword));
+})
